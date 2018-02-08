e6de21b7ba2c79c0305ecefe2e4d47f7f83a6597
char13s <32624173+char13s@users.noreply.github.com>
2018-02-08 16:58:55 -0500

    changed stuff

---
+++ Da Way/Assets/Scripts/AccountSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccountSetup : MonoBehaviour
+{
+	public string savingsBalance;
+	public string checkingBalance;
+
+	public InputField savingsB;
+	public InputField checkingsB;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		savingsBalance = savingsB.text;
+		checkingBalance = checkingsB.text;
+	}
+
+	public void accountUpdate()
+	{
+		
+	}
+}
---
+++ Da Way/Assets/Scripts/SetupBalances.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupBalances : MonoBehaviour
+{
+
+	public int savingsBalance= 50000;
+	public double checkingsBalance= 0.08;
+
+	public Text sB;
+	public Text cB;
+
+	void Start ()
+	{
+		
+	}
+	
+
+	void Update ()
+	{
+		sB.text = "Savings Balance: " + savingsBalance.ToString();
+
+		cB.text = "Checkings Balance: " + checkingsBalance.ToString("F");
+	}
+}
